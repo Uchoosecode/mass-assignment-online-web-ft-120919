0a139315b14731e268bb768410eaaa54f3107203
Ayako Wiltshire <awittyl44@gmail.com>
2020-01-02 17:25:45 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,5 +3,5 @@ class Person
             :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
             
   def initialize(attributes)
-    attributes.each {|
+    attributes.each {|key, value| 
 end
